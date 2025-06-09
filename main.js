70d4a11f25e433cb6b6c27b7bf528afe51fe0cdb
somia <somiadeu2003@gmail.com>
2025-06-10 00:01:28 +0300

    js

---
+++ main.js
@@ -0,0 +1,27 @@
+// console.log("hello world")
+// document.write("<h1>ali hassan maxmuud</h1>")
+// alert("welcome bitch!")
+
+// let age = 20;
+// age = 30;
+// console.log(age)
+
+
+// var ge = 20;
+// age = 30;
+// console.log(age)
+
+
+// const Age = 20;
+// Age = 30;
+// console.log(Age)
+
+
+let name = "somia";
+phone = 610572259;
+email = "somidesu@gmailcom";
+console.log("my name is", name , "my number is ", phone , "my email is", email)
+
+
+
+document.write("somia", " " ,610572259, " ","somidesu@gmailcom")
